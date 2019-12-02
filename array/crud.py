2fc69731b41d216d85551a325e2e92a63da70b50
RJ <rjseven007@gmail.com>
2019-12-02 09:48:12 +0000

    Array's Crud Features

--- array/crud.py
+++ array/crud.py
@@ -1,4 +1,33 @@
-# Create & Read & Update & & Delete
+# Create & Read & Update & Delete
 
-def add(array, new_element):
-    return array.append(new_element)
+
+def add(array, new_element, position=None):
+    if not position:
+        return array.append(new_element)
+    else:
+        return array.insert(position, new_element)
+
+
+def get(array, position=None):
+    if not position:
+        return array[-1]
+    else:
+        return array[position]
+
+
+def update(array, new_element, position=None):
+    if not position:
+        array[-1] = new_element
+        return array
+    else:
+        array[position] = new_element
+        return array
+
+
+def delete(array, position=None):
+    if not position:
+        del(array[-1])
+        return array
+    else:
+        del(array[position])
+        return array
